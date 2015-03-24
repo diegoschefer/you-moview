2d8c53bf86bfe6c6d767a2b435d047b8a2fdf3ff
Diego Schefer <diegoschefer@gmail.com>
2015-03-24 21:38:40 +0000

    remove db conf

--- app/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=200.98.140.15;dbname=movie',
-    'username' => 'root',
-    'password' => 'q1w2e3r4',
-    'charset' => 'utf8',
-];
